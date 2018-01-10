1bd857422fd06b88dd600a7a9e2b2724c2b1815e
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-01-10 22:14:23 +0100

    Changed time.js and OSinfo.js and program.js

--- app/program.js
+++ app/program.js
@@ -1,8 +1,6 @@
-process.stdin.setEncoding('utf-8');
-
 var OSinfo = require('../modules/OSinfo');
 
-
+process.stdin.setEncoding('utf-8');
 console.log('Hello, give the command')
 
 process.stdin.on('readable', function() {
@@ -24,7 +22,7 @@ process.stdin.on('readable', function() {
 				OSinfo.print();
 				break;
 		 	default:
-			process.stderr.write('Wrong instruction!');
+				process.stderr.write('Wrong instruction!');
 		}
 	}
 });
